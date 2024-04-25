9b8212baff35a2cd3889722532a88805d377e727
Daniel Vasti <danielvasticr@gmail.com>
2024-04-25 15:36:39 -0300

    first commit

---
+++ tensorbasics.py
@@ -0,0 +1,43 @@
+import torch
+import numpy as np
+
+
+
+#x = torch.empty(2, 3) - 2dimensions
+#x = torch.empty(2, 2, 3) = 3dimensions
+#print(x)
+
+#x = torch.rand(2,3)
+#print(x)
+
+#x = torch.zeros(1,1)
+#print(x)
+
+#x = torch.ones(2,2)
+#print(x)
+
+#x = torch.ones(2,2, dtype=torch.float16)
+#print(x.dtype)
+
+#x = torch.tensor([2.5, 0.1])
+#print(x)
+
+#x = torch.rand(2,2)
+#y = torch.rand(2,2)
+#y.add_(x)
+#print(y)
+#print(x)
+#print(y)
+#z = x + y
+#z = torch.add(x,y)
+#z = x - y
+#z = torch.sub(x,y)
+#z = x * y
+#z = torch.mul(x,y
+#y.mul_(x)
+#print(z)
+
+#x = torch.rand(4 ,4)
+#print(x)
+#y = x.view(-1, 8)
+#print(y.size())
